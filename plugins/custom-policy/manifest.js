e5ae9fe42fbebf4c7579ea8bac54b2070be84638
kush <ujjwalkaushik24@gmail.com>
2024-12-19 21:34:36 +0530

    Response transformer to add response time

--- plugins/custom-policy/manifest.js
+++ plugins/custom-policy/manifest.js
@@ -1,7 +1,8 @@
 module.exports = {
     version: '0.0.1',
-    policies: ['custom-log', 'rewrite'], // Name of the policy
+    policies: ['custom-log', 'rewrite', 'custom-response'], // Name of the policy
     init: (pluginContext) => {
+      pluginContext.registerPolicy(require('./custom-response-policy'));
       pluginContext.registerPolicy(require('./custom-log-policy'));
       pluginContext.registerPolicy(require('./rewrite-policy'));
     }
